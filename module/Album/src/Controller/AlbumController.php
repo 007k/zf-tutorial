caa782e75d3fec7dee4adf0fe8765b5444b4c88f
007k <007kdcs@gmail.com>
2017-06-09 19:08:21 +0530

    initial save

---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,43 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+// Add the following import:
+use Album\Model\AlbumTable;
+
+class AlbumController extends AbstractActionController
+{
+    // Add this property:
+    private $table;
+
+    // Add this constructor:
+    public function __construct(AlbumTable $table)
+    {
+        $this->table = $table;
+    }
+
+    public function indexAction()
+    {
+        echo "kishan";
+        return $this->table->fetchAll();
+        die('kishan kk');
+        // return new ViewModel([
+        //     'albums' => $this->table->fetchAll(),
+        // ]);
+
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
